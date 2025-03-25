10bf54fc23fedbfad81014f8eaf636e58571d43d
Adrian <adrian.toscano.developer@gmail.com>
2025-03-24 20:40:49 -0700

    implementing nav on brand click

---
+++ src/javascript/event_listeners.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.logo-card').forEach(function(card) {
+    card.addEventListener('click', function() {
+        openModal();
+    });
+});
+
+function openModal() {
+    window.location.href = 'test.html';
+}
