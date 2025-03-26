f4f430324125b01cdc7704f6f1444cf8ecc7f40f
Adrian <adrian.toscano.developer@gmail.com>
2025-03-25 20:26:33 -0700

    added modal on button click

--- src/javascript/modal.js
+++ src/javascript/modal.js
@@ -7,12 +7,12 @@ function openModal() {
 
 // Close the modal
 closeBtn.onclick = function () {
-    modal.style.display = "none";
+  modalWindow.style.display = 'none';
 };
 
-  // Close the modal when clicking outside of it
-  window.onclick = function (event) {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
+// Close the modal when clicking outside of it
+window.onclick = function (event) {
+  if (event.target === modalWindow) {
+    modalWindow.style.display = 'none';
+  }
 };
