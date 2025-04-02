a0e84ed1640805ffd8471d7b8a224ae9b4e945db
Adrian <adrian.toscano.developer@gmail.com>
2025-04-01 21:06:08 -0700

    update functionality for class

--- temporaryFolder/Product.js
+++ temporaryFolder/Product.js
@@ -1,5 +1,12 @@
 class Product {
     constructor() {
-        this.chips = [];
+        this.brand = "";
+        this.upc = -1;
+        this.type = "";
+        this.price = 0.0;
+        this.size = "";
+        this.subName = "";
+        this.flavor = "";
+        this.each = 0; 
     }
 }
---
+++ temporaryFolder/Products.js
@@ -0,0 +1,9 @@
+class Products {
+    constructor() {
+        this.Products = []
+    }
+
+    add(product) {
+        this.push(product);
+    }
+}
