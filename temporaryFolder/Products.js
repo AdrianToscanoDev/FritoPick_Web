67cc859a54a0e6476fef6a748d01819391856ef6
AdrianToscanoDev <adrian.toscano.developer@gmail.com>
2025-04-02 19:28:31 -0700

    hardcoded inventory for testing

--- temporaryFolder/Products.js
+++ temporaryFolder/Products.js
@@ -1,12 +1,28 @@
 class Products {
     constructor() {
         // create a list of all the products
-        this.Products = []
-
-
-    }
-
-    add(product) {
-        this.push(product);
+        this.Products = [
+            /* 
+                brand, upc, type,price,size, subName, flavor, eaches
+            */
+            ["Bakenets", 111111, "chip", 0.0, "XL", "none", "Hot'n Spicy", 15],
+            ["Bakenets", 222222, "chip", 0.0, "XL", "none", "Traditional", 15],
+            ["Bakenets", 333333, "chip", 0.0, "XL", "none", "Flamin Hot", 15],
+            ["Bakenets", 444444, "chip", 0.0, "XL", "none", "Honey Mustard", 15],
+            ["Bakenets", 555555, "chip", 0.0, "XL", "none", "Queso", 15],
+            ["Cheetos", 666666, "chip", 0.0, "XL", "none", "Flamin Hot", 10],
+            ["Cheetos", 777777, "chip", 0.0, "XL", "none", "Flamin Hot Limon", 10],
+            ["Cheetos", 888888, "chip", 0.0, "XL", "none", "Crunchy", 10],
+            ["Doritos", 999999, "chip", 0.0, "XL", "none", "Nacho Cheese", 7],
+            ["Doritos", 100000, "chip", 0.0, "XL", "none", "Cool Ranch", 7],
+            ["Doritos", 100001, "chip", 0.0, "XL", "none", "Spicy Nacho", 7],
+            ["Lays", 100002, "chip", 0.0, "XL", "none", "Classic", 12],
+            ["Lays", 100003, "chip", 0.0, "XL", "none", "BBQ", 12],
+            ["Lays", 100004, "chip", 0.0, "XL", "none", "Limon", 12],
+            ["Lays", 100005, "chip", 0.0, "XL", "none", "Sour Cream & Onion", 12],
+            ["Ruffles", 100006, "chip", 0.0, "XL", "none", "Original", 12],
+            ["Ruffles", 100006, "chip", 0.0, "XL", "none", "Cheddar Sour Cream", 12],
+            ["Ruffles", 100006, "chip", 0.0, "XL", "none", "Flamin Hot", 12],
+        ]
     }
 }
